a99711bc1c69bceb61b5e8d54b0d7636a5c6d7ef
jasonlietnguyen@gmail.com <jason123nguyen@gmail.com>
2017-06-12 18:43:33 -0600

    add bootstrap

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  port = 4000
+
+var campgrounds = [
+  { name: "Tuna Creek", image: "https://static.pexels.com/photos/188940/pexels-photo-188940.jpeg" },
+  { name: "Rock Mountain", image: "https://static.pexels.com/photos/25543/pexels-photo-25543.jpg" },
+  { name: "Turtle Lake", image: "https://static.pexels.com/photos/198979/pexels-photo-198979.jpeg" },
+  { name: "Tuna Creek", image: "https://static.pexels.com/photos/30915/pexels-photo-30915.jpg" },
+  { name: "Rock Mountain", image: "https://static.pexels.com/photos/192518/pexels-photo-192518.jpeg" },
+  { name: "Turtle Lake", image: "https://static.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg" }
+]
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("landing")
+})
+
+app.get("/campgrounds", function (req, res) {
+  res.render("campgrounds", { campgrounds: campgrounds })
+})
+
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new")
+})
+
+app.post("/campgrounds", function (req, res) {
+  campgrounds.push({name: req.body.name, image: req.body.image})
+  res.redirect("/campgrounds")
+})
+
+
+app.listen(port, function (req, res) {
+  console.log("App is listening on port: ", port)
+})
